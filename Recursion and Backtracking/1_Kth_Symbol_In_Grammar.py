771fbdc2c9c52b7e34b33e31911affb6fa81c2d1
uday <uvs2111@gmail.com>
2024-01-15 13:53:57 +0530

    initial commit

--- Recursion and Backtracking/1_Kth_Symbol_In_Grammar.py
+++ Recursion and Backtracking/1_Kth_Symbol_In_Grammar.py
@@ -1,5 +1,6 @@
 
 def kthgrammar(n,k):
+    # 15/55 test cases passed
     string='0'
 
     for i in range(1,n):
@@ -12,9 +13,33 @@ def kthgrammar(n,k):
         string=new
 
     print(string)
-    return string[k-1]
+    return int(string[k-1])
+
+def kthgrammar1(n,k):
+    # new string = original string + replaced original string
+
+    string='0'
+    for i in range(1,n):
+        length=len(string)
+        for j in range(length):
+            if string[j]=='0': string+='1'
+            else: string+='0'
+
+    print(string)
+    return int(string[k-1])
+
+def kthgrammar2(n,k):
+    if n==1: return 0
+
+    mid=2**(n-2) # observation
+    if k<=mid: # first half
+        return kthgrammar2(n-1,k) # same level with same k
+    else:  # k lies in second half, in 2nd half values are negated of first half so we subtract result from 1 to get negation
+        return 1-kthgrammar2(n-1,k-mid) # k is in second half so we subtract mid from k make it relative
 
 if __name__=="__main__":
     n,k=map(int,input().split())
 
-    print(kthgrammar(n,k))
+    # print(kthgrammar(n,k))
+    # print(kthgrammar1(n,k))
+    print(kthgrammar2(n,k))
