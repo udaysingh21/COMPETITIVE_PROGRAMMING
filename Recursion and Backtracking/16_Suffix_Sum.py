3429adda0d631ca74ef5c56ca06800873a5ba144
uday <uvs2111@gmail.com>
2024-01-21 15:41:48 +0530

    initial commit

--- Recursion and Backtracking/16_Suffix_Sum.py
+++ Recursion and Backtracking/16_Suffix_Sum.py
@@ -1,6 +1,8 @@
+import sys
+sys.setrecursionlimit(10**6)
 def suffixsum(i,j,array):
 
-    if i>=len(array): return 0
+    if i==len(array): return 0
 
     if i>=j:
         return array[i]+suffixsum(i+1,j,array)
@@ -8,8 +10,15 @@ def suffixsum(i,j,array):
         return suffixsum(i+1,j,array)
 
 if __name__=="__main__":
-    N,M=map(int,input().split())
+    N,M=map(int,input().split()) # last m element summation
 
     array=list(map(int,input().split()))
-    i,j=0,N-M
-    print(suffixsum(i,j,array))
+
+    """Python Recursion Failed"""
+    # i,j=0,N-M
+    # print(suffixsum(i,j,array))
+
+    result = 0
+    for i in range(N - M, N):
+        result += array[i]
+    print(result)
